4317a218b9179760167478653358d19993787f69
moon2300 <alhassan965@gmail.com>
2025-03-11 10:05:33 +0100

    add items

---
+++ script.js
@@ -0,0 +1,25 @@
+
+    function addItem() {
+        const slots = document.querySelectorAll('.slot');
+        for (let slot of slots) {
+
+            if (!slot.hasChildNodes()) {
+                const item = document.createElement('div');
+                item.classList.add('item');
+                slot.appendChild(item);
+                break;
+            }
+        }
+    }
+
+const clickableItems = document.querySelectorAll('.clickable-item');
+clickableItems.forEach(item => {
+    item.addEventListener('click', addItem);
+});
+
+
+
+function removeItem(){
+
+}
+
