d5ed0fa54a0842a4f56d4d6805720a07dcf4b69e
StephanieBekker <stephanieduvaldbekker@gmail.com>
2025-03-12 15:35:41 +0100

    Made create items in JavaScript
    Fixed #21

--- script.js
+++ script.js
@@ -1,29 +1,92 @@
 
-function addItem() {
-    const slots = document.querySelectorAll('.slot');
-    for (let slot of slots) {
+//---------------------------------------- Items array with name and color --------------------------------------------//
 
+// Add more if needed to create more items
+const itemTypes = [
+    {
+        name: 'Ruby',
+        color: 'red'
+    },
+    {
+        name: 'Sapphire',
+        color: 'blue'
+    },
+    {
+        name: 'Emerald',
+        color: 'green'
+    }
+
+]
+
+//---------------------------------- Creates Items to place indside inventory slots ----------------------------------//
+
+function createItem(itemType) {
+    // Creates new div element
+    const item = document.createElement('div');
+    // Adds styling from CSS .item
+    item.classList.add('item');
+    // Set background color from itemsTypes
+    item.style.backgroundColor = itemType.color
+    // Saves name as attribute for identification
+    item.dataset.itemName = itemType.name;
+    return item;
+}
+
+
+//--------------------------------------- Creates the Items outside inventory ----------------------------------------//
+
+function createClickableItem() {
+    // Finds items-container div
+    const container = document.querySelector('.item-container')
+
+    // For each ItemsType in the array
+    itemTypes.forEach(itemType => {
+
+        // Makes new clickable dic element
+        const clickableItem = document.createElement('div');
+        // Adds styling from CSS .clickable-item
+        clickableItem.classList.add('clickable-item');
+        // Sets color from itemsTypes array
+        clickableItem.style.backgroundColor = itemType.color;
+
+        // Adds click function that calls addItem with specific itemType
+        clickableItem.onclick =() => addItem (itemType);
+
+        // Adds clickableItem to container
+        container.appendChild(clickableItem);
+    });
+}
+
+
+//---------------------------------------------- Add and Remove Items-------------------------------------------------//
+
+// Adds Item to first empty slot
+function addItem(itemType) {
+    // Finds all inventory slot
+    const slots = document.querySelectorAll('.slot');
+
+    // Looks at all slots
+    for (let slot of slots) {
+        // If slots has no item
         if (!slot.hasChildNodes()) {
-            const item = document.createElement('div');
-            item.classList.add('item');
+            // Makes new item
+            const item = createItem(itemType);
+            // Adds item to empty slot
             slot.appendChild(item);
+            // Stops when first empty slot is found
             break;
         }
     }
 }
 
-const clickableItems = document.querySelectorAll('.clickable-item');
-clickableItems.forEach(item => {
-    item.addEventListener('click', addItem);
-});
 
-
+// Removes items when clicked
 function removeItem(event) {
-// If you click on the item, it should be removes (remove by click on item)
+// If you click directly on item
     if (event.target.classList.contains('item')) {
         event.target.parentElement.removeChild(event.target);
     }
-// Or if you click on the inventory slot with that has an item in it, it removes it (remove by click on slot)
+// Or if you click on a slot that contains an item
     else if (event.target.classList.contains('slot') &&
         event.target.hasChildNodes()) {
         event.target.removeChild(event.target.firstChild);
@@ -31,6 +94,14 @@ function removeItem(event) {
 
 }
 
+
+//---------------------------------------------- Event Listeners -----------------------------------------------------//
+
+// Generate the clickable items
+createClickableItem();
+
+
+// Adds click event to all slots to items can be removed
 const slots = document.querySelectorAll('.slot');
 slots.forEach(slot => {
     slot.addEventListener('click', removeItem);
